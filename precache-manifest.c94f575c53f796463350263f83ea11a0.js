d391f8a33f98efd65edd9bffaa1a601e8f44bf79
csaborio <christian@saborio.org>
2020-10-22 15:09:53 +1100

    Updates

---
+++ precache-manifest.c94f575c53f796463350263f83ea11a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db041ebb4533752e0372425a4e537a3",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "097a313f76ece7b03a8a",
+    "url": "/monster-rolodex/static/css/main.43c790b1.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monster-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monster-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "097a313f76ece7b03a8a",
+    "url": "/monster-rolodex/static/js/main.6ea95e9c.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);

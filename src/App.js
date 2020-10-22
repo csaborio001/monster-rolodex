0963a2764a2c5643194e1d4d4ee9ef5870f88783
csaborio <christian@saborio.org>
2020-10-22 14:57:39 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: [],
+			searchField: ''
+		}
+	}
+
+	componentDidMount() { // Accesible because this is class.
+		fetch('http://jsonplaceholder.typicode.com/users') // Get the data source. 
+		.then( response => response.json() ) // Convert response to JSON.
+		.then( users => this.setState({ monsters: users })) 
+	}
+
+	handleChange = e => {
+			this.setState({searchField : (e.target.value)});
+	}
+
+	render() {
+		/*
+		* Pull props off an object and set them to constants.
+		* Equivalent to writing:
+		* const monsters = this.state.monsters;
+		* const searchField = this.state.searchField;
+		*/
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter(monster =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
+ 			return (
+				<div className="App">
+					<h1>Monster Rolodex</h1>
+					<SearchBox placeholder="Search Monsters"
+							   handleChange={this.handleChange}>
+					</SearchBox>
+					<CardList monsters={filteredMonsters}></CardList>
+			</div>
+		); 
+	}
 }
 
 export default App;
